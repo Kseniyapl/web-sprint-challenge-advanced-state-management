e2becfdc561d23bc5521e03aff428c88aab44b7f
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-11-05 07:41:21 -0400

    added reducer through redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { applyMiddleware, createStore } from 'redux';
+import reducer from './reducers';
+
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
 import "./index.css";
 import App from "./App";
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
+const store = createStore(reducer, applyMiddleware(logger, thunk))
+
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store = {store}>
+        <App />
+    </Provider>, 
     rootElement
 );
 
